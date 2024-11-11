a8096302111f015b7ea1ffe7884a787fe9cc6bb6
Indhiran-god <110456933+Indhiran-god@users.noreply.github.com>
2024-11-11 06:47:58 +0530

    Update orders.js

--- frontend/src/pages/orders.js
+++ frontend/src/pages/orders.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const orders = () => {
   return (
-    <div>orders</div>
+    <div className='bg-white py-2 px-4 flex justify-between items-center'>
+    <h2 className='font-bold text-lg'>All Orders</h2>
+</div>
   )
 }
-
 export default orders
